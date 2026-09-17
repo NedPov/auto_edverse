f7e609ac8510fc60ca944a36a7534b186205e6ea
Pavel <nedusenkop@gmail.com>
2026-09-17 20:48:19 +0500

    Локаторы главной страницы

---
+++ locators/home_locators.py
@@ -0,0 +1,17 @@
+class HomeLocators:
+    # Локаторы для главной страници
+    
+    LOGIN_BUTTON = "[data-qa-id='login_page_button']"
+    
+    LAST_MOVIES = "Последние фильмы"
+    
+    ALL_MOVIES = "Все фильмы"
+    
+    CINESCOPE = "Cinescope"
+    
+    # Блок с фильмами где 12 карточек
+    BLOK_MOVIES = "h2 > div"
+    
+    MORE = "[data-qa-id='more_button']"
+    
+    MORE_MOVIES = ("link", "Показать еще")
